03422efd2e52ca9673073b8ee8f6b52a3f4d83b4
Patrick <patrickburns2557@gmail.com>
2023-02-27 19:02:31 -0600

    Medical tab done, billing tab almost done

--- GUI/PatientDetailedView.py
+++ GUI/PatientDetailedView.py
@@ -68,18 +68,17 @@ class PatientDetailedView(tk.Frame):
         self.shownTab.destroy()
         self.shownTab = PersonalInfoTab(self, self.patient)
         self.shownTab.grid(row=1, column=0, sticky="news")
-        #self.shownTab.pack()
 
     def switchMedical(self):
         self.personalButton.configure(bg=BUTTONUNSELECTED)
         self.medicalButton.configure(bg=BUTTONSELECTED)
         self.billingButton.configure(bg=BUTTONUNSELECTED)
 
-        #prob need to iterate through children and destroy too
+        for i in self.shownTab.winfo_children():
+            i.destroy()
         self.shownTab.destroy()
         self.shownTab = MedicalInfoTab(self, self.patient)
         self.shownTab.grid(row=1, column=0, sticky="news")
-        #self.shownTab.pack()
 
     def switchBilling(self):
         self.personalButton.configure(bg=BUTTONUNSELECTED)
@@ -90,7 +89,6 @@ class PatientDetailedView(tk.Frame):
         self.shownTab.destroy()
         self.shownTab = BillingInfoTab(self, self.patient)
         self.shownTab.grid(row=1, column=0, sticky="news")
-        #self.shownTab.pack()
 
 
 
@@ -241,7 +239,7 @@ class MedicalInfoTab(tk.Frame):
             text="Family Doctor",
             font=FONTINFO
         )
-        self.famDoctorFrame.grid(row=1, column=0, sticky="w", padx=15, pady=15)
+        self.famDoctorFrame.grid(row=1, column=0, sticky="nw", padx=15, pady=15)
         self.famDoctor = tk.Label(
             self.famDoctorFrame,
             text=patient.familyDoctor,
@@ -281,33 +279,232 @@ class MedicalInfoTab(tk.Frame):
         )
         self.discharge.pack()
 
+        #maybe make scrollable at some point
         self.prescriptionFrame = tk.LabelFrame(
             self,
             text="Prescriptions",
             font=FONTINFO
         )
-        self.prescriptionFrame.grid(row=4, column=0, sticky="w", padx=15, pady=15)
+        self.prescriptionFrame.grid(row=1, column=1, sticky="w", padx=15, pady=15, rowspan=2)
         for i in range(len(patient.prescriptionNames)):
             self.prescription = tk.Label(
                 self.prescriptionFrame,
                 relief=tk.RAISED,
-                border=1,
+                borderwidth=1,
                 text=str(i+1) + ".)\n  Name:     " + patient.prescriptionNames[i] + "\n  Amount:   " + patient.prescriptionAmount[i] + "\n  Schedule: " + patient.prescriptionSchedule[i],
                 anchor="w",
                 justify=tk.LEFT,
                 font=FONTINFO
             )
             self.prescription.grid(row=i, column=0, sticky="w", padx=3, pady=3)
+        
+        self.proceduresFrame = tk.LabelFrame(
+            self,
+            text="Scheduled Procedures",
+            font=FONTINFO
+        )
+        self.proceduresFrame.grid(row=3, column=1, sticky="w", padx=15, pady=15)
+        for i in range(len(patient.scheduledProcedures)):
+            self.procedure = tk.Label(
+                self.proceduresFrame,
+                relief=tk.RAISED,
+                borderwidth=1,
+                text=str(i+1) + ".) " + patient.scheduledProcedures[i],
+                anchor="w",
+                justify=tk.LEFT,
+                font=FONTINFO
+            )
+            self.procedure.grid(row=i, column=0, sticky="w", padx=3, pady=3)
+
+        self.doctorNoteFrame = tk.LabelFrame(
+            self,
+            text="Doctor Notes",
+            font=FONTINFO
+        )
+        self.doctorNoteFrame.grid(row=1, column=2, sticky="w", padx=15, pady=15)
+        for i in range(len(patient.doctorNotes)):
+            self.doctorNote = tk.Label(
+                self.doctorNoteFrame,
+                relief=tk.RAISED,
+                borderwidth=1,
+                text=str(i+1) + ".) " + patient.doctorNotes[i],
+                anchor="w",
+                justify=tk.LEFT,
+                font=FONTINFO
+            )
+            self.doctorNote.grid(row=i, column=0, sticky="w", padx=3, pady=3)
+
+        self.nurseNoteFrame = tk.LabelFrame(
+            self,
+            text="Nurse Notes",
+            font=FONTINFO
+        )
+        self.nurseNoteFrame.grid(row=2, column=2, sticky="w", padx=15, pady=15)
+        for i in range(len(patient.nurseNotes)):
+            self.nurseNote = tk.Label(
+                self.nurseNoteFrame,
+                relief=tk.RAISED,
+                borderwidth=1,
+                text=str(i+1) + ".) " + patient.nurseNotes[i],
+                anchor="w",
+                justify=tk.LEFT,
+                font=FONTINFO
+            )
+            self.nurseNote.grid(row=i, column=0, sticky="w", padx=3, pady=3)
+
+        self.addNoteFrame = tk.LabelFrame(
+            self,
+            text="Add New Note",
+            font=FONTINFO
+        )
+        self.note = tk.StringVar()
+        self.addNoteFrame.grid(row=3, column=2, sticky="w", padx=15, pady=15)
+        self.addNoteEntry = tk.Entry(
+            self.addNoteFrame,
+            font=FONTINFO,
+            width=40,
+            textvariable=self.note
+        )
+        self.addNoteEntry.grid(row=0, column=0)
+        self.addNoteButton = tk.Button(
+            self.addNoteFrame,
+            text="Add",
+            font=FONTINFO,
+            padx=5,
+            pady=5
+        )
+        #Create if statement to change between adding doctor note or nurse note
+        #depending on the type of user logged in
+        self.addNoteButton.configure(command=lambda: self.addDoctorNote(patient, self.note.get()))
+        self.addNoteButton.grid(row=0, column=1)
+
+
+    def addNurseNote(self, patient, nurseNote):
+        patient.addNurseNote(nurseNote)
+        self.master.switchMedical()
+    def addDoctorNote(self, patient, doctorNote):
+        patient.addDoctorNote(doctorNote)
+        self.master.switchMedical()
+
+
+        
+
 
 
 class BillingInfoTab(tk.Frame):
     def __init__(self, parentWidget, patient):
-        super().__init__(parentWidget, bg="blue")
+        super().__init__(parentWidget, bg=BGCOLOR)
 
-        self.testLabel = tk.Label(
+        self.nameFrame = tk.LabelFrame(
             self,
-            text="I am the Billing Information Tab",
-            font=("Segoe UI", 25)
+            text="Name",
+            font=FONTINFO
         )
+        self.nameFrame.grid(row=0, column=0, columnspan=10, sticky="w", padx=15)
+        self.name = tk.Label(
+            self.nameFrame,
+            text=patient.firstName + " " + patient.middleName + " " + patient.lastName,
+            font=FONTNAME
+        )
+        self.name.pack()
 
-        self.testLabel.pack()
+        self.insuranceFrame = tk.LabelFrame(
+            self,
+            text="Insurance Carrier",
+            font=FONTINFO
+        )
+        self.insuranceFrame.grid(row=1, column=0, sticky="w", padx=15, pady=15)
+        self.insuranceName = tk.Label(
+            self.insuranceFrame,
+            text=patient.insuranceCarrier,
+            anchor="w",
+            justify=tk.LEFT,
+            font=FONTINFO
+        )
+        self.insuranceName.pack()
+
+        self.policyInfoFrame = tk.LabelFrame(
+            self,
+            text="Insurance Policy Info",
+            font=FONTINFO
+        )
+        self.policyInfoFrame.grid(row=2, column=0, sticky="w", padx=15, pady=15)
+        self.policyInfo = tk.Label(
+            self.policyInfoFrame,
+            text="Account Number: " + patient.insuranceAccountNumber + "\nGroup Number:   " + patient.insuranceGroupNumber,
+            anchor="w",
+            justify=tk.LEFT,
+            font=FONTINFO
+        )
+        self.policyInfo.pack(fill=tk.BOTH)
+
+        self.amountPaidFrame = tk.LabelFrame(
+            self,
+            text="Amount Paid",
+            font=FONTINFO
+        )
+        self.amountPaidFrame.grid(row=3, column=0, sticky="w", padx=15, pady=15)
+        self.amountPaid = tk.Label(
+            self.amountPaidFrame,
+            text="${:.2f}".format(patient.amountPaid),
+            anchor="w",
+            justify=tk.LEFT,
+            font=FONTINFO
+        )
+        self.amountPaid.pack(fill=tk.BOTH)
+
+        self.amountPaidInsuranceFrame = tk.LabelFrame(
+            self,
+            text="Amount Paid By Insurance",
+            font=FONTINFO
+        )
+        self.amountPaidInsuranceFrame.grid(row=4, column=0, sticky="w", padx=15, pady=15)
+        self.amountPaidInsurance = tk.Label(
+            self.amountPaidInsuranceFrame,
+            text="${:.2f}".format(patient.amountPaidByInsurance),
+            anchor="w",
+            justify=tk.LEFT,
+            font=FONTINFO
+        )
+        self.amountPaidInsurance.pack(fill=tk.BOTH)
+
+        patient.updateAmountOwed() #UPDATE AMOUNT OWED BEFORE PRINTING IT IN GUI
+        self.amountOwedFrame = tk.LabelFrame(
+            self,
+            text="Amount Owed",
+            font=FONTINFO
+        )
+        self.amountOwedFrame.grid(row=5, column=0, sticky="w", padx=15, pady=15)
+        self.amountOwed = tk.Label(
+            self.amountOwedFrame,
+            text="${:.2f}".format(patient.amountOwed),
+            anchor="w",
+            justify=tk.LEFT,
+            font=FONTINFO
+        )
+        self.amountOwed.pack(fill=tk.BOTH)
+
+        self.chargesList = tk.StringVar(value=patient.listCharges)
+        self.chargesAmountList = tk.StringVar(value=patient.listChargesAmount)
+        self.listChargesFrame = tk.LabelFrame(
+            self,
+            text="List of Charges",
+            font=FONTINFO
+        )
+        self.listChargesFrame.grid(row=1, column=1, sticky="nw", padx=15, pady=15, rowspan=10)
+        self.chargesListBox = tk.Listbox(
+            self.listChargesFrame,
+            listvariable=self.chargesList,
+            width=30,
+            font=FONTINFO
+        )
+        self.chargesListBox.grid(row=0, column=0, sticky="w", padx=3, pady=3)
+        self.chargesAmountListBox = tk.Listbox(
+            self.listChargesFrame,
+            listvariable=self.chargesAmountList,
+            width=12,
+            font=FONTINFO
+        )
+        self.chargesAmountListBox.grid(row=0, column=1, sticky="w", padx=3, pady=3)
+
+
