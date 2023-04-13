95360ce576c68081142fbd644e2d02bf218c0110
madi621 <madison61801@gmail.com>
2023-04-13 17:01:52 -0500

    Load database info based on type of user logged in

--- Data/assignData.py
+++ Data/assignData.py
@@ -1,5 +1,5 @@
 # create list of patient objects from database info
- 
+import Data.System
 from Data.sqlConnector import *
 from Data.dataClasses import *
 
@@ -25,56 +25,21 @@ class patientList():
         # get current amount of patients
         rowCount = db.checkRowCount()
 
+        # get user type
+        userType = Data.System.getUserType()
+
         for i in range(rowCount):
 
             patients = Patient()
 
-            # getting personal info
+            # information that all login types can view
+
+            # patient name
             patients.setFirstName((setData("first_name")[i])[0])
             patients.setLastname((setData("last_name")[i])[0])
             patients.setMiddleName((setData("middle_name")[i])[0])
 
-            address = []
-            address.append((setData("address_street")[i])[0])
-            address.append((setData("address_city")[i])[0])
-            address.append((setData("address_state")[i])[0])
-            address.append((setData("address_zip")[i])[0])
-            patients.setAddress(address)
-
-            patients.setHomePhone((setData("home_phone")[i])[0])
-            patients.setWorkPhone((setData("work_phone")[i])[0])
-            patients.setMobilePhone((setData("mobile_phone")[i])[0])
-
-            patients.addEmergencyContact((setData("emergency1_name")[i][0]), (setData("emergency1_number")[i][0]))
-            patients.addEmergencyContact((setData("emergency2_name")[i][0]), (setData("emergency2_number")[i][0]))
-
-            # getting medical info
-
-            patients.setDateAdmittance((setData("date_of_admittance")[i][0]))
-            patients.setTimeAdmittance((setData("time_of_admittance")[i][0]))
-            patients.setReasonAdmission((setData("reason_for_admission")[i][0]))
-
-            patients.setFamilyDoctor((setData("family_doctor")[i][0]))
-            patients.dateDischarge = ((setData("discharge_date")[i])[0])
-            patients.timeDischarge = ((setData("discharge_time")[i])[0])
-
-            patients.addDoctorNote((setData("doctor_treatment_notes")[i][0]))
-            patients.addNurseNote((setData("nurse_treatment_notes")[i][0]))
-
-            prescriptionName = (setData("prescription_name")[i][0]).split('\n')
-            prescriptionAmounts = (setData("prescription_amount")[i][0]).split('\n')
-            prescriptionTime = (setData("prescription_schedule")[i][0]).split('\n')
-
-            for j in range(len(prescriptionName)):
-                patients.addPrescription(prescriptionName[j], prescriptionAmounts[j], prescriptionTime[j])
-
-            procedures = (setData("scheduled_procedures")[i][0]).split(', ')
-
-            for j in range(len(procedures)):
-                patients.addScheduledProcedure(procedures[j])
-
-            # getting location info
-
+            # location
             location = []
             location.append((setData("facility_name")[i])[0])
             location.append((setData("floor_number")[i])[0])
@@ -82,6 +47,11 @@ class patientList():
             location.append((setData("bed_number")[i])[0])
             patients.setLocation(location)
 
+            # emergency contacts
+            patients.addEmergencyContact((setData("emergency1_name")[i][0]), (setData("emergency1_number")[i][0]))
+            patients.addEmergencyContact((setData("emergency2_name")[i][0]), (setData("emergency2_number")[i][0]))
+
+            # allowed visitor amount and names
             patients.setNumAllowedVisitors((setData("allowed_visitor_amount")[i])[0])
             visitors = (setData("allowed_visitor_names")[i][0]).split('\n')
             num = int(patients.numAllowedVisitors)
@@ -89,22 +59,77 @@ class patientList():
             for j in range(num):
                 patients.addAllowedVisitor(visitors[j])
 
-            patients.setInsuranceCarrier((setData("insurance_carrier")[i])[0])
-            patients.setInsuranceAccountNumber((setData("insurance_account_number")[i])[0])
-            patients.setInsuranceGroupNumber((setData("insurance_group_number")[i])[0])
 
-            # getting billing info
+            # information that everyone but volunteers may access
+            if userType == 0 or userType == 1 or userType == 2:
 
-            names = (setData("billing_items_name")[i][0]).split('\n')
-            amounts = (setData("billing_items_amount")[i][0]).split('\n')
+                # address
+                address = []
+                address.append((setData("address_street")[i])[0])
+                address.append((setData("address_city")[i])[0])
+                address.append((setData("address_state")[i])[0])
+                address.append((setData("address_zip")[i])[0])
+                patients.setAddress(address)
 
-            for j in range(len(names)):
-                amounts[j] = float(amounts[j])
-                patients.addCharge(names[j], amounts[j])
+                # phone numbers
+                patients.setHomePhone((setData("home_phone")[i])[0])
+                patients.setWorkPhone((setData("work_phone")[i])[0])
+                patients.setMobilePhone((setData("mobile_phone")[i])[0])
 
-            patients.setAmountOwed((setData("amount_owed")[i])[0])
-            patients.setAmountPaid((setData("amount_paid")[i])[0])
-            patients.setAmountPaidByInsurance((setData("paid_by_insurance")[i])[0])
+                # insurance information
+                patients.setInsuranceCarrier((setData("insurance_carrier")[i])[0])
+                patients.setInsuranceAccountNumber((setData("insurance_account_number")[i])[0])
+                patients.setInsuranceGroupNumber((setData("insurance_group_number")[i])[0])
+
+
+                # billing info
+                names = (setData("billing_items_name")[i][0]).split('\n')
+                amounts = (setData("billing_items_amount")[i][0]).split('\n')
+
+                for j in range(len(names)):
+                    amounts[j] = float(amounts[j])
+                    patients.addCharge(names[j], amounts[j])
+
+                patients.setAmountOwed(float((setData("amount_owed")[i])[0]))
+                patients.setAmountPaid(float((setData("amount_paid")[i])[0]))
+                patients.setAmountPaidByInsurance(float((setData("paid_by_insurance")[i])[0]))
+
+            # information that only doctors and nurses may view
+            if userType == 0 or userType == 1:
+
+                # admission info
+
+                patients.setDateAdmittance((setData("date_of_admittance")[i][0]))
+                patients.setTimeAdmittance((setData("time_of_admittance")[i][0]))
+                patients.setReasonAdmission((setData("reason_for_admission")[i][0]))
+
+                # discharge info
+
+                patients.setFamilyDoctor((setData("family_doctor")[i][0]))
+                patients.dateDischarge = ((setData("discharge_date")[i])[0])
+                patients.timeDischarge = ((setData("discharge_time")[i])[0])
+
+                # treatment notes
+
+                patients.addDoctorNote((setData("doctor_treatment_notes")[i][0]))
+                patients.addNurseNote((setData("nurse_treatment_notes")[i][0]))
+
+                # prescription info
+
+                prescriptionName = (setData("prescription_name")[i][0]).split('\n')
+                prescriptionAmounts = (setData("prescription_amount")[i][0]).split('\n')
+                prescriptionTime = (setData("prescription_schedule")[i][0]).split('\n')
+
+                for j in range(len(prescriptionName)):
+                    patients.addPrescription(prescriptionName[j], prescriptionAmounts[j], prescriptionTime[j])
+
+                # schedules procedures info
+
+                procedures = (setData("scheduled_procedures")[i][0]).split(', ')
+
+                for j in range(len(procedures)):
+                    patients.addScheduledProcedure(procedures[j])
+
 
             # list of all patient objects
             self.patientRecords.append(patients)
